1fcbe24aa824c8e03f3805bf56de3f35d7bdef54
SMARTCSE <47376568+SMARTCSE@users.noreply.github.com>
2019-03-08 02:52:19 -0800

    Create integers.java

---
+++ integers.java
@@ -0,0 +1,21 @@
+import java.util.*;
+Class Ideone
+//Main
+{
+public class sum of integers
+{
+public static void main(String[] args)
+{
+int N,K;
+for(N=2;N>0;N++)
+{
+System.Out.println("Enter the N integers");
+}
+for(K= 1 2 3 4 5)
+{
+System.out.println("Enter the K integers");
+System.out.println("Enter the sum of the first two intergers in K");
+}
+}
+}
+}
